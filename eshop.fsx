7b5b0a24d8a427bf00dfacbe1672e816a6180fe2
Malo Archimbaud <malo.archimbaud@algosup.com>
2022-01-18 10:24:47 +0100

    Items + Basket

--- eshop.fsx
+++ eshop.fsx
@@ -1,14 +1,41 @@
-type Menu = 
-    | Language
-    | Currency
-    | Basket
-    | Account
-    | Topitems
-
-type Topitems =
+type ItemsName =
     | Doll
     | Toaster
     | Stick
-    | Film
+    | Movie
     | Mouse
     | Phone
+    | Binks
+    | Poney
+
+type ItemPrice =
+    | DollPrice
+    | ToasterPrice
+    | StickPrice
+    | BinksPrice
+    | PhonePrice
+    | PoneyPrice
+
+type Items = ItemsName * ItemPrice
+
+type Info = 
+    | PhoneInfo
+
+type DetailledItems = ItemsName * ItemPrice * Info
+
+type Account =
+    | Name
+    | Adress
+
+type BasketButtons = 
+    | Remove
+    | Pay
+
+type BasketDisplay = 
+    | ItemsName
+    | ItemsPrice
+    | Quantity
+    | Total
+    | GrandTotal
+
+type Basket = BasketDisplay * BasketButtons 
